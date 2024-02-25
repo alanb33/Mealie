2d1f4237189971f00e9cb305aa05cffa4ada48ae
alanb33 <ab33@protonmail.com>
2024-02-25 16:09:55 -0500

    Initial layout of food database view

--- Mealie/MealieApp/urls.py
+++ Mealie/MealieApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "Mealie"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("debugger/", views.debugger, name="debugger")
+    path("debugger/", views.debugger, name="debugger"),
+    path("view-food-db/", views.view_food_db, name="view-food-db"),
 ]
--- Mealie/MealieApp/views.py
+++ Mealie/MealieApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import FoodItem, JournalEntry
 
 # Create your views here.
 
@@ -8,8 +9,21 @@ def index(request):
         "title": "Mealie!",
     })
 
+def view_food_db(request):
+
+    food_db = FoodItem.objects.all()
+    return render(request, "MealieApp/view_food_db.html",
+    {
+        "title": "View Food Database",
+        "food_db": food_db,
+    })
+
 def debugger(request):
+
+    debug_msg = str(len(FoodItem.objects.all()))
+
     return render(request, "Mealieapp/debugger.html",
     {
         "title": "Debugger Page",
+        "debug_msg": debug_msg,
     })
